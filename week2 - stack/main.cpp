fc6ec6fdcbee29cd39f6c80b4313d2b35aa24903
15Mitko15 <127412477+15Mitko15@users.noreply.github.com>
2023-10-17 15:03:08 +0300

    Add files via upload

---
+++ week2 - stack/main.cpp
@@ -0,0 +1,11 @@
+//#include "Stack.h"
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "docktest.h"
+#include "StackTests.h"
+
+//int main()
+//{
+//	Stack<int> testSt;
+//	testSt.push(1);
+//}
